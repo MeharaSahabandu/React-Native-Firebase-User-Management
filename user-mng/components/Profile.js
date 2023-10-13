9a465e9186794fd6e7719dc9872005abd9ac83e5
MeharaSahabandu <sahabandumehara@gmail.com>
2023-10-13 17:57:56 +0530

    profile update working

--- user-mng/components/Profile.js
+++ user-mng/components/Profile.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { StyleSheet, Text, View, TextInput, Button, Image } from "react-native"; // Import Image
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 import {
   collection,
   doc,
@@ -9,20 +9,73 @@ import {
   deleteDoc,
 } from "firebase/firestore";
 import { db } from "./config.jsx";
-import bck from "../assets/bck.png"; // Import your image
+import Toast from "react-native-toast-message";
 
 export default function Profile() {
+  const [name, setName] = useState("Mehara");
+  const [email, setEmail] = useState("mhr@gmail.com");
+  const [phone, setPhone] = useState("0412225698");
+
+  const handleUpdate = () => {
+    // Your update logic here
+    updateDoc(doc(db, "users", "AJ9Gdgl68XcI5lboIW8J"), {
+      name: name,
+      email: email,
+      phone: phone,
+    })
+      .then(() => {
+        console.log("User Details Updated Successfully");
+        showToast("User Details Updated Successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const showToast = (message) => {
+    Toast.show({
+      type: "success",
+      position: "bottom",
+      text1: message,
+      visibilityTime: 3000, // 3 seconds
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Image source={bck} style={styles.image} /> {/* Add Image */}
-      <Text style={styles.topicReg}>John Doe</Text><br/><br/><br/><br/><br/>
-      <TextInput value="" placeholder="Name" style={[styles.input, styles.inputWidth]} /><br/>
-      <TextInput value="" placeholder="Email" style={[styles.input, styles.inputWidth]} /><br/>
-      <TextInput value="" placeholder="Phone Number" style={[styles.input, styles.inputWidth]} /><br/>
-      <TextInput value="" placeholder="Password" style={[styles.input, styles.inputWidth]} /><br/><br/>
+      <Text style={styles.topicReg}>John Doe</Text>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <TextInput
+        value={name}
+        placeholder="Name"
+        onChangeText={setName}
+        style={[styles.input, styles.inputWidth]}
+      />
+      <br />
+      <TextInput
+        value={email}
+        placeholder="Email"
+        onChangeText={setEmail}
+        style={[styles.input, styles.inputWidth]}
+      />
+      <br />
+      <TextInput
+        value={phone}
+        placeholder="Phone Number"
+        onChangeText={setPhone}
+        style={[styles.input, styles.inputWidth]}
+      />
+      <br />
       <View style={[styles.button, styles.inputWidth]}>
-        <Text style={styles.buttonText}><b>Save Changes</b></Text>
+        <Text style={styles.buttonText} onPress={handleUpdate}>
+          <b>Save Changes</b>
+        </Text>
       </View>
+      <Toast ref={(ref) => Toast.setRef(ref)} />
     </View>
   );
 }
@@ -35,21 +88,21 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   image: {
-    width: 50, // Set the image width
-    height: 0, // Set the image height
+    width: 50,
+    height: 0,
   },
   topicReg: {
     color: "black",
     fontSize: 30,
-    marginTop: "-65%"
+    marginTop: "-65%",
   },
   input: {
     height: 50,
     borderWidth: 2,
     borderColor: "black",
-    color: "black", 
+    color: "black",
     padding: 10,
-    borderRadius: 10
+    borderRadius: 10,
   },
   inputWidth: {
     width: "80%",
